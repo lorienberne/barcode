fc90b70a8d4df3554c1bbdb02d10719bc701ed3f
JMS <jose.sabater.iglesias@gmail.com>
2024-03-17 17:45:51 +0100

    add extra CNN layer

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from torch import nn
 
+
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
@@ -7,18 +8,32 @@ class Autoencoder(nn.Module):
             nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(
+                16, 32, kernel_size=3, stride=1, padding=1
+            ),  # New convolutional layer
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 8, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25),  # Dropout added here in the encoder
         )
-        
+
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(
+                8, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),  # Corresponding deconvolution layer
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.Sigmoid()
+            nn.ConvTranspose2d(
+                32, 16, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                16, 1, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.Sigmoid(),
         )
-         
+
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
